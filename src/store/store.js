0686a6ed32baab009ff013ce26046e5ce0f08ef0
winniecl <chongluiwinnie@gmail.com>
2022-09-17 13:07:52 +0800

    categories action redux thunk

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,18 @@ import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
 const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
 );
+const thunkMiddleware = (store) => (next) => (action) => {
+  if (typeof action === "function") {
+    action(dispatch);
+  }
+};
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
     window &&
